705082bc4b384cf190854eddb3f793f88d70b64c
Sam <samuel.waters@gmail.com>
2023-05-04 16:17:03 -0400

    chore: completed first test

--- game_logic/ship_logic.js
+++ game_logic/ship_logic.js
@@ -1,5 +1,24 @@
-function checkForShip(){
+function checkForShip( player, coordinates ) {
     // return false
-}
+    let shipPresent, ship;
 
+
+    for(let i = 0; i < player.ships.length; i++){
+        ship = player.ships[i];
+        // console.log(ship.locations)
+        for (let x of ship.locations){
+            if (JSON.stringify(x) === JSON.stringify(coordinates)) {shipPresent = true}
+            else shipPresent = false
+        }
+    }
+    return shipPresent;
+}
+player1 = {
+    ships: [
+        {
+            locations : [[0,0]]
+        }
+    ]
+}
+// console.log(checkForShip(player1, [9,0]))
 module.exports.checkForShip = checkForShip
