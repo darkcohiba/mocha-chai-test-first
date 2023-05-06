c6f1161283a47bdeff415366bc0516e4412474e3
Sam <samuel.waters@gmail.com>
2023-05-06 10:54:34 -0400

    chore: added division test

--- algo/math.js
+++ algo/math.js
@@ -1,3 +1,5 @@
 const addition = (num1, num2) => num1 + num2;
 
-module.exports = { addition }
+const divide = (num1, num2) => num1 / num2;
+
+module.exports = { addition, divide };
--- test/algo_test.js
+++ test/algo_test.js
@@ -1,9 +1,18 @@
 const expect = require('chai').expect
 
 describe("Math Tests", ()=>{
-    const addition = require("../algo/math").addition
+    const math = require("../algo/math")
     it("Addition function should return two numbers summed", ()=>{
-        expect(addition(2, 8)).to.be.equal(10);
-        expect(addition(8, 8)).to.be.equal(16);
+        expect(math.addition(8, 8)).to.be.equal(16);
+        expect(math.addition(2, 8)).to.be.equal(10);
     })
+
+    it("'Divided' function should return two numbers divided", ()=>{
+        expect(math.divide(8, 8)).to.be.equal(1);
+        expect(math.divide(16, 8)).to.be.equal(2);
+    })
+
+
+
+
 })
