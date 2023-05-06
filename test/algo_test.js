be6aedebfa403865333dc0a74e81b71b519c7de0
Sam <samuel.waters@gmail.com>
2023-05-06 10:56:02 -0400

    chore: added division test

--- test/algo_test.js
+++ test/algo_test.js
@@ -5,6 +5,8 @@ describe("Math Tests", ()=>{
     it("Addition function should return two numbers summed", ()=>{
         expect(math.addition(8, 8)).to.be.equal(16);
         expect(math.addition(2, 8)).to.be.equal(10);
+        expect(math.addition(2, "8")).to.be.equal("28");
+
     })
 
     it("'Divided' function should return two numbers divided", ()=>{
