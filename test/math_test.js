f14bebeda1b006c2d361a2138f4e5b4aae53b914
Sam <samuel.waters@gmail.com>
2023-05-09 17:46:54 -0600

    chore: exponent

--- test/math_test.js
+++ test/math_test.js
@@ -6,7 +6,6 @@ describe("Math Tests", ()=>{
         expect(math.addition(8, 8)).to.be.equal(16);
         expect(math.addition(2, 8)).to.be.equal(10);
         expect(math.addition(2, "8")).to.be.equal("28");
-
     })
 
     it("'Divided' function should return two numbers divided", ()=>{
