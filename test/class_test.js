f8f575b6d5040d73e94736bb9dfccb63e3f460ad
Sam <samuel.waters@gmail.com>
2023-06-15 10:06:40 -0600

    chore: adding assert and should

--- test/class_test.js
+++ test/class_test.js
@@ -1,10 +1,16 @@
 const expect = require('chai').expect
+const assert = require('chai').assert
+require('chai').should()
+
 // const should = require('chai').should()
 
 describe("Class Tests", ()=>{
     const math = require("../algo/class")
     it("should multiply two numbers together and return the number", ()=>{
         expect(math.multiply(4, 4)).to.equal(16)
+        assert.equal(math.multiply(2, 3), 6)
+        assert.equal(math.multiply(10, 3), 10*3)
+        math.multiply(2, 3).should.equal(6);
     })
 
     it('should return the sum of two numbers', ()=>{
--- test/first_test.js
+++ test/first_test.js
@@ -2,7 +2,9 @@ const expect = require('chai').expect
 
 // suite
 describe('Mocha', function(){
+    // unit test
     it("should run our tests using npm", ()=>{
+        // assertation
         expect(true).to.be.ok;
     })
 })
